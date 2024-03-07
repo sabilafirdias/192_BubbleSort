55318a6b2756bf1b9a5429f49bb36027ff528c32
sabilafiridas <sabilafirdias@gmail.com>
2024-03-07 11:49:07 +0700

    menyelesaikan prosedur bubblesort dan membuat pemanggil

--- bubblesort ASD/bubblesort ASD.cpp
+++ bubblesort ASD/bubblesort ASD.cpp
@@ -40,9 +40,28 @@ void display() {        // Prosedur untuk menampilkan hasil
     cout << endl;                                           // Output baris kosong
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void bubbleSortArray() {                      // Prosedur untuk mengurutkan array dengan metode bubble sort
+    int pass = 1;                             // Step 1
+
+    for (pass; pass <= n - 1; pass + 1) {             // Looping dengan i dimulai dari 1 hingga n-1
+        for (int j = 0; j <= n - 1 - pass; j++) {     // Looping dengan j dimulai dari 0 hingga n-1
+            if (a[j] > a[j + 1]) {                    // Jika nilai pada a[j] lebih besar dari a[j+1]
+                int temp = a[j];                      // Simpan nilai a[j] ke variabel sementara temp
+                a[j] = a[j + 1];                      // Assign nilai a[j+1] ke a[j]
+                a[j + 1] = temp;                      // Assign nilai temp ke a[j+1]
+            }
+        }
+    }
 }
+
+int main() {
+    input();                // Memanggil read()
+    bubbleSortArray();      // Memanggil bubblesortarray()
+    display();              // Memanggil display()
+    return 0;
+}
+
+
+
 
 
