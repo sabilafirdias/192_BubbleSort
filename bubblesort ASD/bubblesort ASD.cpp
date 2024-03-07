c41d5f321341299cf2b1dad511985c00057ceb64
sabilafiridas <sabilafirdias@gmail.com>
2024-03-07 11:28:04 +0700

    membuat prosedur untuk menampilkan hasil

--- bubblesort ASD/bubblesort ASD.cpp
+++ bubblesort ASD/bubblesort ASD.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int a[20]                // Deklarasi array a dengan ukuran 20
+int a[20];                // Deklarasi array a dengan ukuran 20
 int n;                   // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 void input() {           // Procedure untuk input
@@ -26,6 +26,18 @@ void input() {           // Procedure untuk input
         cout << "Data ke-" << (i + 1) << "; ";      // Output ke layar
         cin >> a[i];                                // Input dari pengguna
     }
+}
+
+void display() {        // Prosedur untuk menampilkan hasil
+    cout << endl;       // Output baris kosong
+    cout << "================================" << endl;     // Output ke layar
+    cout << "Elemen Array yang telah tersusun" << endl;     // Output ke layar
+    cout << "================================" << endl;     // Output ke layar
+    for (int j = 0; j < n; j++) {                           // Looping dengan j dimulai dari 0 hingga n-1
+        cout << "Data Ke-" << j + 1 << ": ";
+        cout << a[j] << endl;                               // Output ke layar
+    }
+    cout << endl;                                           // Output baris kosong
 }
 
 int main()
